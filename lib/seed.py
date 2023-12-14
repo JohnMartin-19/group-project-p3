5e160beb2c03a341d7cdaf778576538ae8324d51
John <johnnkonge2020@gmail.com>
2023-12-14 19:20:46 +0300

    tests modified

--- lib/seed.py
+++ lib/seed.py
@@ -6,7 +6,7 @@ import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Restaurant, Review, Customer
+
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///many_to_many.db')
