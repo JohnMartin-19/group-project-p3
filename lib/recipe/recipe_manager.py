102e5aae8b6bdd625fd101eaa4683da5b95e6a5a
John <johnnkonge2020@gmail.com>
2023-12-15 06:38:11 +0300

    modified removed unnecessary files

--- lib/recipe/recipe_manager.py
+++
@@ -1,9 +0,0 @@
-class RecipeManager:
-    def __init__(self):
-        self.recipes = []
-
-    def add_recipe(self, recipe):
-        self.recipes.append(recipe)
-
-    def get_recipe_by_ingredient(self, ingredient):
-        return [recipe for recipe in self.recipes if ingredient in recipe.ingredients]
