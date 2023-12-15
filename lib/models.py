1ecb6eaf2da56329dd04474e478a6b2e92f051bb
John <johnnkonge2020@gmail.com>
2023-12-15 06:29:00 +0300

    added models

--- lib/models.py
+++ lib/models.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine('sqlite:///many_to_many.db')
 
 Base = declarative_base()
+"""
 user = Table(
     'users',
     Base.metadata,
@@ -13,4 +14,28 @@ user = Table(
     
     extend_existing=True,
 )
+"""
 
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    meal_plans = relationship("MealPlan", back_populates="user")
+
+class Recipe(Base):
+    __tablename__ = 'recipes'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    ingredients = Column(String, nullable=False)
+    instructions = Column(String, nullable=False)
+    meal_plans = relationship("MealPlan", back_populates="recipe")
+
+class MealPlan(Base):
+    __tablename__ = 'meal_plans'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    recipe_id = Column(Integer, ForeignKey('recipes.id'), nullable=False)
+    date = Column(Date, nullable=False)
+    user = relationship("User", back_populates="meal_plans")
+    recipe = relationship("Recipe", back_populates="meal_plans")
